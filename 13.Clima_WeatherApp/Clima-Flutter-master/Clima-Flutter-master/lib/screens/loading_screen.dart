29b74210a7ba16c8e3e647e2a551811b67e01805
jandonggun <donggontrannguyen@gmail.com>
2020-12-13 19:44:00 +0700

    get api

--- 13.Clima_WeatherApp/Clima-Flutter-master/Clima-Flutter-master/lib/screens/loading_screen.dart
+++ 13.Clima_WeatherApp/Clima-Flutter-master/Clima-Flutter-master/lib/screens/loading_screen.dart
@@ -1,4 +1,6 @@
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,12 +9,37 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1');
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            //Get the current location
+            //Get the current
           },
           child: Text('Get Location'),
         ),
